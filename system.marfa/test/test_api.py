26877d4e0cf407e7dba606c6fae0ec8e91e017eb
Yuriy Zhloba <y_zhloba@wargaming.net>
2018-09-07 21:02:32 +0300

    small refactor

--- system.marfa/test/test_api.py
+++ system.marfa/test/test_api.py
@@ -3,54 +3,70 @@
 import requests
 import json
 
-base_url = 'http://localhost:8080/'
+base_url = 'http://localhost:8080'
 headers = {
     'Content-Type': 'application/json'
 }
 
 
 def enter(cat, town):
-    data = json.dumps({
+    data = {
         'cat': cat,
         'town': town
-    })
-    print('> enter', data)
-    res = requests.put(base_url + 'enter', headers=headers, data=data)
-    print('<', res.status_code, res.content)
+    }
+    print_query('enter', data)
+    print_res(requests.put(base_url + '/enter', headers=headers, data=json.dumps(data)))
 
 
 def leave(cat, town):
-    data = json.dumps({
+    data = {
         'cat': cat,
         'town': town
-    })
-    print('> leave', data)
-    res = requests.put(base_url + 'leave', headers=headers, data=data)
-    print('<', res.status_code, res.content)
+    }
+    print_query('leave', data)
+    print_res(requests.put(base_url + '/leave', headers=headers, data=json.dumps(data)))
 
 
 def dwell(cat, town):
-    print('> dwell', cat, town)
-    res = requests.get(base_url + 'dwell?cat=' + cat + '&town=' + town, headers=headers)
-    print('<', res.status_code, res.content)
+    data = {
+        'cat': cat,
+        'town': town
+    }
+    print_query('dwell', data)
+    print_res(requests.get(base_url + '/dwell?cat=' + cat + '&town=' + town, headers=headers))
 
 
 def where_is_cat(cat):
-    print('> where_is_cat', cat)
-    res = requests.get(base_url + 'where_is_cat?cat=' + cat, headers=headers)
-    print('<', res.status_code, res.content)
+    data = {
+        'cat': cat
+    }
+    print_query('where_is_cat', data)
+    print_res(requests.get(base_url + '/where_is_cat?cat=' + cat, headers=headers))
 
 
 def who_is_in_town(town):
-    print('> who_is_in_town', town)
-    res = requests.get(base_url + 'who_is_in_town?town=' + town, headers=headers)
-    print('<', res.status_code, res.content)
+    data = {
+        'town': town
+    }
+    print_query('who_is_in_town', data)
+    print_res(requests.get(base_url + '/who_is_in_town?town=' + town, headers=headers))
 
 
 def ping():
-    print('> ping')
-    res = requests.put(base_url + 'ping', headers=headers)
-    print('<', res.status_code, res.content)
+    print_query('ping', None)
+    print_res(requests.get(base_url + '/ping', headers=headers))
+
+
+def print_query(query, data):
+    print('>', query)
+    if data:
+        print(json.dumps(data, indent=2))
+
+
+def print_res(res):
+    j = json.loads(res.content)
+    print('<', res.status_code)
+    print(json.dumps(j, indent=2), '\n')
 
 
 enter('Tihon', 'Minsk')
