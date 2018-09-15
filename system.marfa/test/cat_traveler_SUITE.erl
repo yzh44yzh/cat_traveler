26e57a41e6aae3897b4edbfb8c7faaed68ce3394
Yuriy Zhloba <y_zhloba@wargaming.net>
2018-09-15 21:40:18 +0300

    move_test, error_paths_test

--- system.marfa/test/cat_traveler_SUITE.erl
+++ system.marfa/test/cat_traveler_SUITE.erl
@@ -10,8 +10,9 @@
 all() ->
     [
         enter_test,
-        leave_test
-        % move_test
+        leave_test,
+        move_test,
+        error_paths_test
     ].
 
 
@@ -52,6 +53,8 @@ enter_test(_Config) ->
     ?assertEqual({ok, false}, cat_traveler:dwell(Tihon, Barcelona)),
     ?assertEqual({ok, false}, cat_traveler:dwell(Marfa, Minsk)),
     ?assertEqual({ok, false}, cat_traveler:dwell(Marfa, Barcelona)),
+    ?assertEqual({ok, Minsk}, cat_traveler:where_is_cat(Tihon)),
+    ?assertEqual({error, not_found}, cat_traveler:where_is_cat(Marfa)),
 
     ok = cat_traveler:enter(Marfa, Barcelona),
     ?assertEqual({error, already_in_town}, cat_traveler:enter(Marfa, Barcelona)),
@@ -61,6 +64,8 @@ enter_test(_Config) ->
     ?assertEqual({ok, false}, cat_traveler:dwell(Tihon, Barcelona)),
     ?assertEqual({ok, false}, cat_traveler:dwell(Marfa, Minsk)),
     ?assertEqual({ok, true}, cat_traveler:dwell(Marfa, Barcelona)),
+    ?assertEqual({ok, Minsk}, cat_traveler:where_is_cat(Tihon)),
+    ?assertEqual({ok, Barcelona}, cat_traveler:where_is_cat(Marfa)),
     ok.
 
 
@@ -97,10 +102,75 @@ leave_test(_Config) ->
     ?assertEqual({ok, false}, cat_traveler:dwell(Marfa, Minsk)),
     ok.
 
-% move_test
-% two cats into one town
-% first cat move other town
-% second cat move other town
 
-% error_path_test
-% check error results for all functions
+move_test(_Config) ->
+    Minsk = cat_traveler_types:new_town(<<"Minsk">>),
+    ?assertEqual({ok, []}, cat_traveler:who_is_in_town(Minsk)),
+    Barcelona = cat_traveler_types:new_town(<<"Barcelona">>),
+    ?assertEqual({ok, []}, cat_traveler:who_is_in_town(Barcelona)),
+
+    Tihon = cat_traveler_types:new_cat(<<"Tihon">>),
+    ?assertEqual({ok, false}, cat_traveler:dwell(Tihon, Minsk)),
+    Marfa = cat_traveler_types:new_cat(<<"Marfa">>),
+    ?assertEqual({ok, false}, cat_traveler:dwell(Marfa, Minsk)),
+
+    ok = cat_traveler:enter(Tihon, Minsk),
+    ok = cat_traveler:enter(Marfa, Minsk),
+    {ok, Cats} = cat_traveler:who_is_in_town(Minsk),
+    ?assert(lists:member(Tihon, Cats)),
+    ?assert(lists:member(Marfa, Cats)),
+    ?assertEqual({ok, []}, cat_traveler:who_is_in_town(Barcelona)),
+    ?assertEqual({ok, true}, cat_traveler:dwell(Tihon, Minsk)),
+    ?assertEqual({ok, true}, cat_traveler:dwell(Marfa, Minsk)),
+    ?assertEqual({ok, false}, cat_traveler:dwell(Tihon, Barcelona)),
+    ?assertEqual({ok, false}, cat_traveler:dwell(Marfa, Barcelona)),
+    ?assertEqual({ok, Minsk}, cat_traveler:where_is_cat(Tihon)),
+    ?assertEqual({ok, Minsk}, cat_traveler:where_is_cat(Marfa)),
+
+    ok = cat_traveler:enter(Tihon, Barcelona),
+    ?assertEqual({ok, [Marfa]}, cat_traveler:who_is_in_town(Minsk)),
+    ?assertEqual({ok, [Tihon]}, cat_traveler:who_is_in_town(Barcelona)),
+    ?assertEqual({ok, false}, cat_traveler:dwell(Tihon, Minsk)),
+    ?assertEqual({ok, true}, cat_traveler:dwell(Marfa, Minsk)),
+    ?assertEqual({ok, true}, cat_traveler:dwell(Tihon, Barcelona)),
+    ?assertEqual({ok, false}, cat_traveler:dwell(Marfa, Barcelona)),
+    ?assertEqual({ok, Barcelona}, cat_traveler:where_is_cat(Tihon)),
+    ?assertEqual({ok, Minsk}, cat_traveler:where_is_cat(Marfa)),
+
+    ok = cat_traveler:enter(Marfa, Barcelona),
+    ?assertEqual({ok, []}, cat_traveler:who_is_in_town(Minsk)),
+    {ok, Cats2} = cat_traveler:who_is_in_town(Barcelona),
+    ?assert(lists:member(Tihon, Cats2)),
+    ?assert(lists:member(Marfa, Cats2)),
+    ?assertEqual({ok, false}, cat_traveler:dwell(Tihon, Minsk)),
+    ?assertEqual({ok, false}, cat_traveler:dwell(Marfa, Minsk)),
+    ?assertEqual({ok, true}, cat_traveler:dwell(Tihon, Barcelona)),
+    ?assertEqual({ok, true}, cat_traveler:dwell(Marfa, Barcelona)),
+    ?assertEqual({ok, Barcelona}, cat_traveler:where_is_cat(Tihon)),
+    ?assertEqual({ok, Barcelona}, cat_traveler:where_is_cat(Marfa)),
+    ok.
+
+
+error_paths_test(_Config) ->
+    Minsk = cat_traveler_types:new_town(<<"Minsk">>),
+    Pinsk = cat_traveler_types:new_town(<<"Pinsk">>),
+    Tihon = cat_traveler_types:new_cat(<<"Tihon">>),
+
+    ?assertEqual(ok, cat_traveler:enter(Tihon, Minsk)),
+    ?assertEqual({error, already_in_town}, cat_traveler:enter(Tihon, Minsk)),
+    ?assertEqual({error, invalid_town}, cat_traveler:enter(Tihon, Pinsk)),
+
+    ?assertEqual(ok, cat_traveler:leave(Tihon, Minsk)),
+    ?assertEqual({error, not_in_town}, cat_traveler:leave(Tihon, Minsk)),
+    ?assertEqual({error, invalid_town}, cat_traveler:leave(Tihon, Pinsk)),
+
+    ?assertEqual({ok, false}, cat_traveler:dwell(Tihon, Minsk)),
+    ?assertEqual({error, invalid_town}, cat_traveler:dwell(Tihon, Pinsk)),
+
+    ?assertEqual({error, not_found}, cat_traveler:where_is_cat(Tihon)),
+    ok = cat_traveler:enter(Tihon, Minsk),
+    ?assertEqual({ok, Minsk}, cat_traveler:where_is_cat(Tihon)),
+
+    ?assertEqual({ok, [Tihon]}, cat_traveler:who_is_in_town(Minsk)),
+    ?assertEqual({error, invalid_town}, cat_traveler:who_is_in_town(Pinsk)),
+    ok.
